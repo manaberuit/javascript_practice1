18f6f6bc381a16270ae6eb338a75700168dafdf4
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-03-18 06:55:42 +0900

    elseを削除

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 for (let i = 1; i < 100; i++) {
   if (i % 15 === 0) {
-    console.log(`FizzBuzz\x20${i}`)
+    console.log(`FizzBuzz\x20${i}`);
   }else if(i % 3 === 0) {
-    console.log(`Fizz\x20${i}`)
+    console.log(`Fizz\x20${i}`);
   }else if(i % 5 === 0) {
-    console.log(`Buzz\x20${i}`)
-  }else {
-    console.log()
+    console.log(`Buzz\x20${i}`);
   }
 }
