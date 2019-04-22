91d14a101607a45d5d07966a8bf13ab95dc4474d
野口慧 <satoru@noguchitoshinoMacBook-Pro.local>
2019-04-22 23:19:56 +0900

    ver1.2

--- sub1.js
+++ sub1.js
@@ -1,30 +1,31 @@
 const number1 = 3;
 const number2 = 5;
 const number3 = number1 * number2;
+const maxNum = 100;
 let fizz = number1;
 let buzz = number2;
-let fizzbazz = number3;
+let fizzbuzz = number3;
 let i = 1;
 let j = 1;
 let k = 1; 
 
-while(i <50){
-    if(fizz < buzz && fizz < fizzbazz){
+while(i < maxNum){
+    if(fizz < buzz){
         console.log("Fizz " + fizz);
         i += 1;
         fizz = number1 * i;
-    }else if(buzz < fizz && buzz < fizzbazz){
+    }else if(buzz < fizz){
         console.log("Bazz " + buzz);
         j += 1;
         buzz = number2 * j;
-    }else if(buzz == fizzbazz && fizz == fizzbazz){
-        console.log("FizzBazz " + fizzbazz)
+    }else if(buzz === fizzbuzz && fizz === fizzbuzz){
+        console.log("FizzBuzz " + fizzbuzz)
         i += 1;
         j += 1;
         k += 1;
         fizz = number1 * i;
         buzz = number2 * j;
-        fizzbazz = number3 * k;
+        fizzbuzz = number3 * k;
     }
 }
 
