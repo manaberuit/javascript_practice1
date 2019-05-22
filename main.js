41f7759e7072be33b0cc5be7028212aa541af14e
making-try <making.try@gmail.com>
2019-05-22 22:52:19 +0900

    local0522-2

--- main.js
+++ main.js
@@ -16,48 +16,33 @@ document.addEventListener('DOMContentLoaded',function(){
             let text = document.createTextNode('整数値を入力してください');
             anchor.appendChild(text);
             output.appendChild(anchor);
-        for(let i = 1; i < 100; i++){
-            const output = document.getElementById('output');
-            const anchor = document.createElement('p');
-            if ((i % Number(fizzNum.value) === 0) && (i % Number(buzzNum.value) === 0)){
-                let text = document.createTextNode("FizzBuzz"+" "+i);
-                anchor.appendChild(text);
-                output.appendChild(anchor);
-            }else if (i % Number(fizzNum.value) === 0){
-                let text = document.createTextNode("Fizz"+" "+i);
-                anchor.appendChild(text);
-                output.appendChild(anchor);
-            }else if (i % Number(buzzNum.value) === 0){
-                let text = document.createTextNode("Buzz"+" "+i);
-                anchor.appendChild(text);
-                output.appendChild(anchor);
         }else{
-            for(let i = 1; i < 100; i++){
-                //const output = document.getElementById('output');
-                //const anchor = document.createElement('p');
-                if ((i % Number(fizzNum.value) === 0) && (i % Number(buzzNum.value) === 0)){
-                    let result = 'FizzBuzz' + ' ' + i;
-                    //anchor.appendChild(text);
-                    //output.appendChild(anchor);
-                }else if (i % Number(fizzNum.value) === 0){
-                    let result = 'Fizz' + ' ' + i;
-                    //anchor.appendChild(text);
-                    //output.appendChild(anchor);
-                }else if (i % Number(buzzNum.value) === 0){
-                    let result = 'Buzz' + ' ' + i;
-                    //anchor.appendChild(text);
-                    //output.appendChild(anchor);
-                }
-                alert(result);
-                if(result){
-                    const output = document.getElementById('output');
-                    const anchor = document.createElement('p');
-                    const text = document.createTextNode(result)
-                    anchor.appendChild(text);
-                    output.appendChild(anchor);
-                    result = NaN
-                }
+          let result = '';
+          for(let i = 1; i < 100; i++){
+            //const output = document.getElementById('output');
+            //const anchor = document.createElement('p');
+            if ((i % Number(fizzNum.value) === 0) && (i % Number(buzzNum.value) === 0)){
+              result = 'FizzBuzz' + ' ' + i;
+              //anchor.appendChild(text);
+              //output.appendChild(anchor);
+            }else if (i % Number(fizzNum.value) === 0){
+              result = 'Fizz' + ' ' + i;
+              //anchor.appendChild(text);
+              //output.appendChild(anchor);
+            }else if (i % Number(buzzNum.value) === 0){
+              result = 'Buzz' + ' ' + i;
+              //anchor.appendChild(text);
+              //output.appendChild(anchor);
             }
+            if(result){
+              const output = document.getElementById('output');
+              const anchor = document.createElement('p');
+              const text = document.createTextNode(result)
+              anchor.appendChild(text);
+              output.appendChild(anchor);
+              result = NaN
+            }
+          }
         }
 
         },false);}
