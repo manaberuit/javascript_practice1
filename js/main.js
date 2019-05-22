78ec5f270f11a10f0c73ae8c2042300313a2f4b8
yusukebkk <kjdaga2@gmail.com>
2019-05-22 18:42:59 +0900

    jacascript変更
    matchをtestに変更

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@
   //submitクリック時にイベントを発生させる
   document.getElementById('submit').addEventListener( 'click',() =>{
     //フォームの中身を受け取る
-    const buzz = document.getElementById("buzz").value;
     const fizz = document.getElementById("fizz").value;
+    const buzz = document.getElementById("buzz").value;
     //数字のみかどうかチェック。非負整数のみ。
-    const buzz_check = buzz.match(/^(0|[1-9][0-9]*)$/);
-    const fizz_check = fizz.match(/^(0|[1-9][0-9]*)$/);
+    const fizz_check = (/^(0|[1-9][0-9]*)$/).test(fizz);
+    const buzz_check = (/^(0|[1-9][0-9]*)$/).test(buzz);
+
     //数字以外が含まれていたら検出してエラーメッセージを表示
-    if (buzz_check === null || fizz_check === null){
+    if (!(fizz_check && buzz_check)){
       document.querySelector("p.message").textContent = "整数値を入力してください"
     //エラーがなければFizzBuzzを実行
     }else{
