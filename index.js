45d98d68acc49e34051c52a92c6323f43a634944
ShiniYanada <ysthon@gmail.com>
2019-05-29 10:07:47 +0900

    キャメルケース、変数宣言、変数名、==から===への修正

--- index.js
+++ index.js
@@ -3,69 +3,67 @@ document.addEventListener('DOMContentLoaded',function(){
     var button = document.querySelector(('#button'));
     var buzz = document.querySelector('#buzz');
     var fizz = document.querySelector('#fizz');
-    var output_box = document.querySelector('.output-box');
+    var outputBox = document.querySelector('.output-box');
 
     button.addEventListener('click', function(){
         
-        while(output_box.firstChild){
-            output_box.removeChild(output_box.firstChild);
+        while(outputBox.firstChild){
+            outputBox.removeChild(outputBox.firstChild);
         }
 
-        var result_arr = []; 
+        var resultArr = []; 
 
         if(!fizz.value.match(/[0-9]/) || !buzz.value.match(/[0-9]/)){
             var p = document.createElement('p');
             p.innerText = '整数値を入力してください';
-            output_box.appendChild(p);
+            outputBox.appendChild(p);
             return;
         }
 
-        var fizz_val = parseInt(fizz.value);
-        var buzz_val = parseInt(buzz.value);
+        var fizzVal = parseInt(fizz.value);
+        var buzzVal = parseInt(buzz.value);
 
-        if(fizz_val == 0 || buzz_val == 0){
+        if(fizzVal === 0 || buzzVal === 0){
             var p = document.createElement('p');
             p.innerText = '１以上の整数値を入力してください';
-            output_box.appendChild(p);
-            return
+            outputBox.appendChild(p);
+            return;
         }
 
-        var i = 1;
-        var j = 1;
+        var multipiedByFizz = 1;
+        var multipiedByBuzz = 1;
         
-        while(fizz_val * i < 100){
-            result_arr.push(fizz_val * i);
-            i++;
+        while(fizzVal * multipiedByFizz < 100){
+            resultArr.push(fizzVal * multipiedByFizz);
+            multipiedByFizz++;
         }
 
-        while(buzz_val * j < 100){
-            result_arr.push(buzz_val * j);
-            j++;
+        while(buzzVal * multipiedByBuzz < 100){
+            resultArr.push(buzzVal * multipiedByBuzz);
+            multipiedByBuzz++;
         }
 
-        result_arr.sort(function(a,b){
+        resultArr.sort(function(a,b){
             return a - b;
         })
 
-        result = result_arr.filter(function(x, i, self){
+        var result = resultArr.filter(function(x, i, self){
             return self.indexOf(x) == i;
         })
 
-        console.log(result);
-
         result.forEach(function(el){
-            if( el % fizz_val == 0 && el % buzz_val == 0){
+            if( el % fizzVal === 0 && el % buzzVal === 0){
                 var p = document.createElement('p');
                 p.innerText = `FizzBuzz ${el}`;
-                output_box.appendChild(p);   
-            } else if ( el % fizz_val == 0){
+                outputBox.appendChild(p);   
+            } else if ( el % fizzVal === 0){
                 var p = document.createElement('p');
                 p.innerText = `Fizz ${el}`;
-                output_box.appendChild(p); 
-            } else if ( el % buzz_val == 0){
+                outputBox.appendChild(p); 
+            } else if ( el % buzzVal === 0){
                 var p = document.createElement('p');
                 p.innerText = `Buzz ${el}`;
-                output_box.appendChild(p); 
+                outputBox.appendChild(p); 
             }
         });
     });
