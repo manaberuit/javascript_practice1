1d79ce075c54480ca006d758050b3b1a2e4780be
Hiroki Fujita <hf.rowkey313@gmail.com>
2019-05-30 17:42:55 +0900

    コードレビュー時の問題点を修正

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-document.querySelector('.js-submit').addEventListener('click', function(e) {
-
+document.getElementById('js-submit').addEventListener('click', function(e) {
+  
   e.preventDefault();
 
   // ユーザー入力値を取得
-  var fizzNum = Number(document.querySelector('.js-input-fizz').value);
-  var buzzNum = Number(document.querySelector('.js-input-buzz').value);
+  var fizzNum = Number(document.getElementById('js-input-fizz').value);
+  var buzzNum = Number(document.getElementById('js-input-buzz').value);
 
   // 結果表示エリアのDOMを取得
-  var resultArea = document.querySelector('.js-result');
+  var resultArea = document.getElementById('js-result');
 
   // 表示する数字の最大値を指定
-  var max = 100;
+  var MAX = 100;
 
   // 出力処理の開始前に、結果表示エリアを空にする
   resultArea.innerHTML = '';
@@ -18,7 +18,7 @@ document.querySelector('.js-submit').addEventListener('click', function(e) {
   // エラーメッセージ
   var errMsg1 = '<p>整数値を入力してください。</p>';
   var errMsg2 = '<p>1以上で入力してください。</p>';
-  var errMsg3 = '<p>' + max + '以下で入力してください。</p>';
+  var errMsg3 = '<p>' + MAX + '以下で入力してください。</p>';
 
 
   // 整数値のバリデーション
@@ -34,7 +34,7 @@ document.querySelector('.js-submit').addEventListener('click', function(e) {
   }
 
   // 最大値のバリデーション
-  if(fizzNum > max || buzzNum > max) {
+  if(fizzNum > MAX || buzzNum > MAX) {
     resultArea.insertAdjacentHTML('afterbegin', errMsg3);
     return;
   }
@@ -42,21 +42,16 @@ document.querySelector('.js-submit').addEventListener('click', function(e) {
 
   // 挿入するHTMLを生成する
   var html = '';
-  for(var i = 1; i <= max; i++) {
+  for(var i = 1; i <= MAX; i++) {
 
     if(i % fizzNum === 0 && i % buzzNum === 0) {
       html += 'FizzBuzz ' + i + '<br>';
-      continue;
-    }
 
-    if(i % fizzNum === 0) {
+    } else if(i % fizzNum === 0) {
       html += 'Fizz ' + i + '<br>';
-      continue;
-    }
 
-    if(i % buzzNum === 0) {
+    } else if(i % buzzNum === 0) {
       html += 'Buzz ' + i + '<br>';
-      continue;
     }
   }
 
