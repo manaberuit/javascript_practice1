e83a32f34b082697ac3ce888a12294ac51120277
ro-g <666ppp41@gmail.com>
2019-06-20 21:48:05 +0900

    javascript課題１　修正後

--- js/test6.js
+++ js/test6.js
@@ -1,16 +1,22 @@
 'use strict'
+//execution-button=実行ボタンとして
+//idを　execution　=> exec-btn　
+//変数名をbutton　=> exceBtnなどとする
+//一目で分かる
+const execBtn = document.getElementById('exec-btn');
 
-const button = document.getElementById('execution');
-
-button.addEventListener('click', function(){
+execBtn.addEventListener('click', function(){
     const Fizz = document.getElementById('Fizz').value;
     const Buzz = document.getElementById('Buzz').value;
     const result = document.getElementById('result');
 
+    //チェック用の正規表現　数字が入力されているか
     const check = /^[0-9]+$/;
 
+    //resultの子要素
     while(result.firstChild)
-    result.removeChild(result.firstChild);
+    result.textContent = ''; 　//=>でもOK！より短く、イメージしやすい
+    // result.removeChild(result.firstChild);
 
     if(check.test(Fizz) && check.test(Buzz) ){
         for(let num = 1; num < 100; num++ ){
