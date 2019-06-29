f7e3186ad6ac0b8bb2f4f9342b57c79ed35902ca
xyytgae <51901346+xyytgae@users.noreply.github.com>
2019-06-29 14:03:10 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+  'use strict';
+
+   {
+      var btn = document.getElementById('btn');
+      var output = document.getElementById('output');
+      btn.addEventListener('click', () => {
+        var fizznum = document.getElementById('fizznum');
+        var buzznum = document.getElementById('buzznum');
+        output.innerText = fizznum;
+      });
+      }
 
