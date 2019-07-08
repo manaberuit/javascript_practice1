70342d640a1407450e3a5613b84e75c64ce43dc5
uetani <r.uetani33@gmail.com>
2019-07-09 04:07:03 +0900

    javascript完成

---
+++ main.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const calcBtnDom = document.getElementById('calc-btn');
+
+// 「実行」ボタンを押す 
+calcBtnDom.addEventListener('click', calcFizzBuzz);
+
+function calcFizzBuzz() {
+    const inputFizzValue = document.getElementById('fizz-input').value;
+    const inputBuzzValue = document.getElementById('buzz-input').value;
+
+    // 整数値の判定
+    if (Math.sign(inputFizzValue) && Math.sign(inputBuzzValue) && (inputFizzValue > 0) && (inputBuzzValue > 0)) {
+        const outputSectionDom = document.getElementById('output-section');
+
+        // #output-sectionの元のテキストを削除
+        outputSectionDom.innerHTML = '';
+
+        // 計算結果の出力html
+        let outputSectionHtml = '';
+
+
+        const outputDomPerth = document.createDocumentFragment();
+
+        // FizzBuzzの表示部分のモジュール
+        for (let num = 1; num < 100; num++) {
+            let loopSaveHtml = '';
+            let loopSaveName = '';
+            let loopSaveNum = '';
+
+            // Fizzの倍数なら出力
+            if ((num % inputFizzValue) == 0) {
+                loopSaveName += 'Fizz';
+                loopSaveNum = num;
+            }
+            if ((num % inputBuzzValue) == 0) {
+                loopSaveName += 'Buzz';
+                loopSaveNum = num;
+            }
+            loopSaveHtml = loopSaveName + ' ' + loopSaveNum;
+
+            if (loopSaveHtml) {
+                // outputSectionHtml += loopSaveHtml + "\n";
+                let createLowDom = document.createElement('div');
+                let createLowDomText = document.createTextNode(loopSaveHtml);
+                createLowDom.appendChild(createLowDomText);
+                outputDomPerth.appendChild(createLowDom);
+            }
+        }
+
+        // FizzBuzzで作ったDomをまとめてパース
+        outputSectionDom.appendChild(outputDomPerth);
+
+        // FizzBuzzで作ったDomをまとめてパース
+        outputSectionDom.appendChild(outputDomPerth);
+
+
+
+
+    }
+
+}
