cd3cdfa6bca2242f8c62cad2b230b9678715f3d8
kankan <sunagakannta7@gmail.com>
2019-07-31 22:23:36 +0900

    refactor:べっちさん指摘取り込み

--- function.js
+++ function.js
@@ -1,73 +1,86 @@
-document.getElementById("execution-button").onclick = function() {
+document.getElementById("execution-button").addEventListener('click', FizzBuzz, false);
+
+function FizzBuzz () {
 
   const Fizz = document.getElementById("fizz-area").value
   const Buzz = document.getElementById("buzz-area").value
-  var target = document.getElementById("result-area");
+  const target = document.getElementById("result-area");
 
   if (isValid(Fizz, Buzz)){
-
-    var fizzBuzzArray = creatFizzBuzzArray(Fizz, Buzz)
-    var textFizzBuzzArray = createIncludeTextFizzBuzzArray(fizzBuzzArray) 
-    displayFizzBuzz(textFizzBuzzArray, target)
-    // 全体的にクラスで書きたい
+    const fizzBuzzArray = creatFizzBuzzArray(Fizz, Buzz);
+    const textFizzBuzzArray = createIncludeTextFizzBuzzArray(fizzBuzzArray, Fizz, Buzz); 
+    displayFizzBuzz(textFizzBuzzArray, target);
   }else{
     disPlayAlert(target);
   }
-
 }
   
 function isValid(fizz, buzz) {
-  return ( !! Number(fizz) && !! Number(buzz) ) ? true : false 
+  return ( !! Number(fizz) && !! Number(buzz) ) ? true : false; 
 }
 
 function creatFizzBuzzArray(fizz, buzz) {
-  var standardNum = Math.max(fizz,buzz)
-  var nums = []
+  const standardNum = Math.max(fizz,buzz)
+  const nums = []
   for ( i = 1; standardNum * i < 100;  i++ ) {
     nums.push( fizz * i);
     nums.push( buzz * i);
   }
-  var fizzBuzzNums = nums.filter(function (x, i, self) {
-    return self.indexOf(x) === i;
+  const fizzBuzzNums = nums.filter(function (element, index, self) {
+    return self.indexOf(element) === index;
   });
   return fizzBuzzNums.sort(function (a, b) {
     return a - b;
   });
 }
 
-function createIncludeTextFizzBuzzArray(fizzBuzzAry) {
-  var htmlAray = []
+function createIncludeTextFizzBuzzArray(fizzBuzzAry, fizz, buzz) {
+  const htmlAray = []
   fizzBuzzAry.forEach(function(element) {
-    if ( isNaNFizzBuzz(element) ) {
-      htmlAray.push( ('FizzBuzz' + element))
-    } else if (isNaNFizz(element)){
-      htmlAray.push( ('Fizz' + element))
-    }else if( isNaNBuzz(element) ){
-      htmlAray.push( ('Buzz' + element))
+    switch (whichFizzBuzzType(element, fizz, buzz)) {
+      case 'FizzBuzz':
+        htmlAray.push( ('FizzBuzz' + element));
+        break;
+      case 'Fizz':
+        htmlAray.push( ('Fizz' + element));
+        break;
+      case 'Buzz':
+        htmlAray.push( ('Buzz' + element));
+        break;
     }
   });
   return htmlAray
 }
 
+function whichFizzBuzzType(num, fizz, buzz) {
+  if ( isNaNFizzBuzz(num, fizz, buzz) ) {
+    return 'FizzBuzz';
+  } else if (isNaNFizz(num, fizz)){
+    return 'Fizz';
+  }else if( isNaNBuzz(num, buzz )){
+    return 'Buzz';
+  }
+}
+
 function isNaNFizz(num) {
-  var Fizz = document.getElementById("fizz-area").value
-  return (num % Fizz == 0) ? true : false
+  const Fizz = document.getElementById("fizz-area").value
+  return (num % Fizz == 0) ? true : false;
 }
 
 function isNaNBuzz(num) {
-  var Buzz = document.getElementById("buzz-area").value
-  return (num % Buzz == 0) ? true : false
+  const Buzz = document.getElementById("buzz-area").value
+  return (num % Buzz == 0) ? true : false;
 }
 
 function isNaNFizzBuzz(num) {
-  return (isNaNFizz(num) && isNaNBuzz(num)) ? true : false
+  return (isNaNFizz(num) && isNaNBuzz(num)) ? true : false;
 }
 
 function displayFizzBuzz(fizzBuzzAry, target) {
   target.innerHTML = null;
   fizzBuzzAry.forEach(function(element) {
-    text = document.createElement('p')
-    text.textContent = element
+    text = document.createElement('p');
+    text.textContent = element;
     target.appendChild(text);
   });
 }
