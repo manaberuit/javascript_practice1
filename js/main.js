3ba19fc8595bf2eec175897a5ea2542c7467aa0a
shimon <shimondetective@gmail.com>
2019-08-18 19:24:04 +0900

    バリデーションの処理：追加

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const sendBtn=document.getElementById('send-btn');
+const sendBtn = document.getElementById('send-btn');
 
 sendBtn.addEventListener('click', function() {
 	const fizz = document.getElementById('fizz').value;
@@ -8,13 +8,16 @@ sendBtn.addEventListener('click', function() {
 	const target = document.getElementById('output');
 	target.innerHTML = "";
 
-	for(let i = 1; i <= 100; i++){
-	  if(i % fizz === 0 && i % buzz === 0){
-	    target.insertAdjacentHTML('beforeend',`<div>FizzBuzz ${i}</div>`);
-	  }else if(i % fizz === 0){
-	    target.insertAdjacentHTML('beforeend',`<div>Fizz ${i}</div>`);
-	  }else if(i % buzz === 0){
-	    target.insertAdjacentHTML('beforeend',`<div>Buzz ${i}</div>`);
-	  }
+	if(isFinite(fizz) && isFinite(buzz) && fizz !== "" && buzz !== ""){
+		for(let i = 1; i <= 100; i++){
+		  if(i % fizz === 0 && i % buzz === 0){
+		    target.insertAdjacentHTML('beforeend',`<div>FizzBuzz ${i}</div>`);
+		  }else if(i % fizz === 0){
+		    target.insertAdjacentHTML('beforeend',`<div>Fizz ${i}</div>`);
+		  }else if(i % buzz === 0){
+		    target.insertAdjacentHTML('beforeend',`<div>Buzz ${i}</div>`);
+		  }
+		}
+	}else{target.insertAdjacentHTML('beforeend',`整数値を入力してください`);
 	}
 });
