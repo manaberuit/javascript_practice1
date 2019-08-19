e540d2342d4c67b3ecc57d8453309ecc93fe6496
shimon <shimondetective@gmail.com>
2019-08-19 17:57:44 +0900

    数値チェック修正

--- js/main.js
+++ js/main.js
@@ -3,12 +3,14 @@
 const sendBtn = document.getElementById('send-btn');
 
 sendBtn.addEventListener('click', function() {
-	const fizz = document.getElementById('fizz').value;
-	const buzz = document.getElementById('buzz').value;
+	const fizz = parseFloat(document.getElementById('fizz').value);
+	const buzz = parseFloat(document.getElementById('buzz').value);
 	const target = document.getElementById('output');
 	target.innerHTML = "";
 
-	if(isFinite(fizz) && isFinite(buzz) && fizz !== "" && buzz !== ""){
+	if(isNaN(fizz) || isNaN(buzz)){
+		target.insertAdjacentHTML('beforeend',`整数値を入力してください`);
+	}else{
 		for(let i = 1; i <= 100; i++){
 		  if(i % fizz === 0 && i % buzz === 0){
 		    target.insertAdjacentHTML('beforeend',`<div>FizzBuzz ${i}</div>`);
@@ -18,6 +20,5 @@ sendBtn.addEventListener('click', function() {
 		    target.insertAdjacentHTML('beforeend',`<div>Buzz ${i}</div>`);
 		  }
 		}
-	}else{target.insertAdjacentHTML('beforeend',`整数値を入力してください`);
 	}
 });
