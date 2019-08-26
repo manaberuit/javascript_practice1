c7cfbfb1780551f1df78ef0e3eea2a8c5ef9a49f
kubo158 <kubo158@hotmail.com>
2019-08-26 17:45:45 +0900

    190826 kadai.js

--- kadai.js
+++ kadai.js
@@ -1,6 +1,4 @@
-'use strict';
-
-{
+'use strict'; {
 
   //ボタンの要素を取得
   let btn = document.querySelector('button');
@@ -11,46 +9,22 @@
   // 結果表示のpの要素を取得
   let elem = document.getElementById('elem');
 
-  //fizzInput,buzzInputの要素を取得
-  // let mathFizz = document.getElementById('fizzInput');
-  // let mathBuzz = document.getElementById('buzzInput');
-
-// querySelectorAll
-
   //実行キーを押す
   btn.addEventListener('click', () => {
 
-    if(document.getElementsByClassName('pItem')) {
-      //何か処理を書く
-      console.log('true');
-      console.log(document.getElementsByClassName('pItem'));
-
-      // pItemのclass要素が存在する場合は削除。
-        let removeChilds = document.getElementsByClassName('pItem');
-        while(removeChilds.length) {
-          removeChilds.item(0).remove();
-        }
-      } else {
-        console.log('false');}
-
+  //一度取得したクラスの削除
+    let removeChilds = document.getElementsByClassName('pItem');
+    while (removeChilds.length) {
+      removeChilds.item(0).remove();
+    }
 
 
     // 取得したinput要素の入力値を取得。
-    let fizzInputValue = parseInt(fizzInput.value);
-    let buzzInputValue = parseInt(buzzInput.value);
+    let fizzInputValue = parseFloat(fizzInput.value);
+    let buzzInputValue = parseFloat(buzzInput.value);
 
 
-
-    //もしtextboxで小数入力された場合、parseIntした整数を入力ボックスに書き換えたいのですが方法が分かりません。
-    // mathFizz.textContent = fizzInputValue;
-    // mathBuzz.textContent = buzzInputValue;
-
-
-    // もし、全角数字を入力した場合、半角数字に変換した後、作業を続行したいのですが、
-    // 半角数字への変換方法が分かりません。
-
-
-    if (isNaN(fizzInputValue) || isNaN(buzzInputValue)) {
+    if (!Number.isInteger(fizzInputValue) || !Number.isInteger(buzzInputValue)) {
       elem.textContent = '数値を入力';
     } else if (fizzInputValue <= 0 || buzzInputValue <= 0) {
       elem.textContent = '';
@@ -58,19 +32,16 @@
       elem.textContent = '';
       for (let i = 1; i < 100; i++) {
         if (i % (fizzInputValue * buzzInputValue) === 0) {
-          // console.log(`FizzBuzzの値：${i}`);
           let pItem = document.createElement('p');
           pItem.classList.add('pItem');
           pItem.textContent = (`FizzBuzzの値： ${i}`);
           output.appendChild(pItem);
         } else if (i % fizzInputValue === 0) {
-          // console.log(`Fizzの値：${i}`);
           let pItem = document.createElement('p');
           pItem.classList.add('pItem');
           pItem.textContent = (`Fizzの値： ${i}`);
           output.appendChild(pItem);
         } else if (i % buzzInputValue === 0) {
-          // console.log(`Buzzの値：${i}`);
           let pItem = document.createElement('p');
           pItem.classList.add('pItem');
           pItem.textContent = (`Buzzの値： ${i}`);
