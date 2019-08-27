2561caa13975346e292e3036b8945da7ebd73131
kubo158 <kubo158@hotmail.com>
2019-08-27 18:40:21 +0900

    190827 kadai.html

--- kadai.js
+++ kadai.js
@@ -1,28 +1,26 @@
 'use strict'; {
 
   //ボタンの要素を取得
-  let btn = document.querySelector('button');
+  const btn = document.querySelector('button');
 
   // 結果表示の要素を取得
-  let output = document.getElementById('output');
+  const output = document.getElementById('output');
 
   // 結果表示のpの要素を取得
-  let elem = document.getElementById('elem');
+  const elem = document.getElementById('elem');
 
   //実行キーを押す
   btn.addEventListener('click', () => {
 
   // 一度取得したクラスの削除
-    // let removeChilds = document.getElementsByClassName('pItem');
-    // while (removeChilds.length) {
-    //   removeChilds.item(0).remove();
-    // }
-    elem.textContent = '';
+      // elem.textContent = '';
+      while(elem.firstChild) {
+      elem.removeChild(elem.firstChild);
+      }
 
     // 取得したinput要素の入力値を取得。
     let fizzInputValue = parseFloat(fizzInput.value);
     let buzzInputValue = parseFloat(buzzInput.value);
-
 
     if (!Number.isInteger(fizzInputValue) || !Number.isInteger(buzzInputValue)) {
       elem.textContent = '数値を入力';
@@ -31,21 +29,18 @@
     } else {
       elem.textContent = '';
       for (let i = 1; i < 100; i++) {
-        if (i % (fizzInputValue * buzzInputValue) === 0) {
-          let pItem = document.createElement('p');
-          pItem.classList.add('pItem');
-          pItem.textContent = (`FizzBuzzの値： ${i}`);
-          output.appendChild(pItem);
+        if ( i % (fizzInputValue * buzzInputValue) === 0) {
+          let p = document.createElement('p');
+          p.textContent = (`FizzBuzzの値： ${i}`);
+          elem.appendChild(p);
         } else if (i % fizzInputValue === 0) {
-          let pItem = document.createElement('p');
-          pItem.classList.add('pItem');
-          pItem.textContent = (`Fizzの値： ${i}`);
-          output.appendChild(pItem);
+          let p = document.createElement('p');
+          p.textContent = (`Fizzの値： ${i}`);
+          elem.appendChild(p);
         } else if (i % buzzInputValue === 0) {
-          let pItem = document.createElement('p');
-          pItem.classList.add('pItem');
-          pItem.textContent = (`Buzzの値： ${i}`);
-          output.appendChild(pItem);
+          let p = document.createElement('p');
+          p.textContent = (`Buzzの値： ${i}`);
+          elem.appendChild(p);
         }
       }
     }
