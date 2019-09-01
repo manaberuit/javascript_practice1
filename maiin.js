7ed7f72fde70775a337e8c03d8d441575dd099c0
Lago <masa.lago.r128@gmail.com>
2019-09-01 23:02:50 +0900

    parseFloatの追加とファイルの編集

--- maiin.js
+++ maiin.js
@@ -1,53 +1,31 @@
 `use strict`;
 
 
-//  const btn = document.getElementById(`btn`);
-//  const fizz =document.getElementById(`fizz`);
-//  const buzz =document.getElementById(`buzz`);
 
-//  const fizzbuzz = document.createElement(`fizzbuzz`);
-//    fizzbuzz.classList.add(`fizzbuzz`);
-
-
-//  btn.addEventListener(`click`,()=>{
-//    fizzbuzz.fizzbuzz.textContent = `整数値を入力してください`;
-
-//  })
-  // btn.addEventListener(`click`,()=>{
-  //   document.text.textContent ='整数値を入力してください';
-  // })
   {
     const text = document.getElementById(`text`);
     const btn = document.getElementById(`btn`);
     const fizz = document.getElementById(`fizz`);
     const buzz = document.getElementById(`buzz`);
 
-      // btn.onclick = function(){
+
         btn.addEventListener(`click`,() =>{
 
 
 
         text.innerHTML = "";
 
-        let fizzNum = fizz.value;
-        let buzzNum = buzz.value;
-        // var sum = fizz.value * buzz.value;
-        // text.textContent = sum;
+        const fizzNum = parseFloat(fizz.value);
+        const buzzNum = parseFloat(buzz.value);
 
 
-
-        // return;
-        for (const num = 1 ;num < 100; num++){
+        for (let num = 1 ;num < 100; num++) {
 
           if (!Math.floor(fizzNum) || !Math.floor(buzzNum)){
-            // text.innerHTML = ("整数値を入力してください");
-            return;
+            text.innerHTML = ("整数値を入力してください");
           }
 
-          if (fizzNum === "" || buzzNum === ""){
-            // text.innerHTML = ("整数値を入力してください");
-            return;
-          }
+
 
           if (num % fizzNum === 0 && num % buzzNum === 0){
             text.innerHTML += (`FizzBuzz${num}<br>`);
@@ -58,15 +36,6 @@
           }
 
 
-          // if (Math.floor(fizzNum) || Math.floor(buzzNum)){
-          //   text.innerHTML = ("整数値を入力してください");
-          //   return;
-          // }else if (fizzNum === "" || buzzNum === ""){
-          //   text.innerHTML = ("整数値を入力してください");
-          //   return;
-          // }
         }
       })
-
-
   }
