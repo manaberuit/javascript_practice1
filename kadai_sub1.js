0e095d9b449cfedb13c581682eab023be1233069
hayato0128 <54713105+hayato0128@users.noreply.github.com>
2019-09-02 21:59:31 +0900

    【修正】kadai_sub1
    レビューありがとうございます。
    修正したのでお時間ある時にご確認お願いいたします。

--- kadai_sub1.js
+++ kadai_sub1.js
@@ -1,9 +1,9 @@
-for(let number=1;number<=99;number++){
-    if(number%15===0){
+for(let number = 1; number <= 99; number++){
+    if(number % (5*3) === 0){
         console.log(`fizzbuzz ${number}`);
-    } else if (number%5===0){
+    } else if (number % 5 === 0){
         console.log(`buzz ${number}`);
-    } else if (number%3===0){
+    } else if (number % 3 === 0){
         console.log(`fizz ${number}`);
-}
+    }
 }
