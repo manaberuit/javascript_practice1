4769e3c0e500a088968a41bc585536f83b209ba2
hayato0128 <54713105+hayato0128@users.noreply.github.com>
2019-09-08 13:49:19 +0900

    【JavaScript 課題１】FizzBuzz問題
    完成しましたのでご確認お願いします。

--- kadai.js
+++ kadai.js
@@ -1,24 +1,25 @@
 window.onload = function() {
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn"); 
+
 btn.addEventListener("click",function(){
 const fizz = parseInt(document.getElementById("fizz").value);
-const buzz = parseInt(document.getElementById("buzz").value); 
-const output = doucument.getElementById("output").innerHTML = "";
+const buzz = parseInt(document.getElementById("buzz").value);
+const output = document.getElementById("output");
+output.innerHTML = "";
 
+if(isNaN(fizz) || isNaN(buzz)){
+  output.innerHTML = "整数値を入力してください";
+} else {
   for(let i = 1; i < 100; i++){
       if(i % fizz === 0 && i % buzz === 0){
-        output.innerHTML = "`FizzBuzz ${i}`";
-      // console.log(`FizzBuzz ${i}`);
+        output.innerHTML += `FizzBuzz ${i}` + "<br>";
     } else if(i % fizz === 0){
-      output.innerHTML = "`Fizz ${i}`";
-      // console.log(`Fizz ${i}`);
+      output.innerHTML += `Fizz ${i}` + "<br>";
     } else if(i % buzz === 0){
-      output.innerHTML = "`Buzz ${i}`";
-      // console.log(`Buzz ${i}`);
-    } else{
-      output.innerHTML = "整数値を入力してください";
-      // console.log("整数値を入力してください");
+      output.innerHTML += `Buzz ${i}` + "<br>";
+    }
     }
   }
   });
 }
+
