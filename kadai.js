6366a495899a9ab10fb47e16160400da28d0bb03
hayato0128 <54713105+hayato0128@users.noreply.github.com>
2019-09-10 09:07:14 +0900

    【JavaScript 課題１】FizzBuzz問題
    インデント修正しました。

--- kadai.js
+++ kadai.js
@@ -7,10 +7,10 @@ const buzz = parseInt(document.getElementById("buzz").value);
 const output = document.getElementById("output");
 output.innerHTML = "";
 
-if(isNaN(fizz) || isNaN(buzz)){
+  if(isNaN(fizz) || isNaN(buzz)){
   output.innerHTML = "整数値を入力してください";
-} else {
-  for(let i = 1; i < 100; i++){
+  } else {
+    for(let i = 1; i < 100; i++){
       if(i % fizz === 0 && i % buzz === 0){
         output.innerHTML += `FizzBuzz ${i}` + "<br>";
     } else if(i % fizz === 0){
@@ -20,6 +20,6 @@ if(isNaN(fizz) || isNaN(buzz)){
     }
     }
   }
-  });
+});
 }
 
