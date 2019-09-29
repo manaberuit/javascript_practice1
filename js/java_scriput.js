f58e3ac924ea0ff970680f6da2f92181b8cc0448
takuma yoshida <kusunoki-50105@infoseek.jp>
2019-09-29 23:03:57 +0900

    "file re-edit"

--- js/java_scriput.js
+++ js/java_scriput.js
@@ -1,14 +1,8 @@
 'use strict';
 
 {
-    // let tmp_list = [];
-    //     for(let i = 1; i <= 10; i++){
-    //         tmp_list.push(i);
-    //     }
-    // return tmp_list;
-    let i;
-    for (i = 1; i <= 99; i++){
-        if (i % 3 == 0 && i % 5 == 0) {
+    for (let i = 1; i > 100; i++){
+        if (i % 3 === 0 && i % 5 === 0) {
             console.log(`FizzBuzz ${i}`);
         } else if (i % 3 == 0) {
             console.log(`Fizz ${i}` );
