af5e2ae8ed13af39af70c69790b22fe4e16a4925
takuma yoshida <kusunoki-50105@infoseek.jp>
2019-10-06 20:53:42 +0900

    "Correction ver1"

--- js/java_scriput.js
+++ js/java_scriput.js
@@ -3,27 +3,27 @@
 {
     document.getElementById("btn").addEventListener("click", function(){
         // HTMLから入力された値を取得する
-        let fizz = Number(document.getElementById("fizz").value);
-        let buzz = Number(document.getElementById("buzz").value);
-        // バリデーション用変数。0以上の整数のみ通す。
-        let numerical_check = /^([1-9]\d*|0)$/;
+        const FIZZ = Number(document.getElementById("fizz").value);
+        const BUZZ = Number(document.getElementById("buzz").value);
+        // バリデーション用変定数。0以上の整数のみ通す。
+        const NUMERICAL_CHECK = /^([1-9]\d*|0)$/;
         // メッセージ用変数。
-        let output_result = "";
+        let Output_Result = "";
 
          // バリデーション用変数の頭に！をつけて条件がそろえばfalseを返しFizzBuzzの処理を開始する。
-        if (!numerical_check.test(fizz) || !numerical_check.test(buzz)){
-            output_result = "<p>整数値を入力してください</p>";
+        if (!NUMERICAL_CHECK.test(FIZZ) || !NUMERICAL_CHECK.test(BUZZ)){
+            Output_Result = "<p>整数値を入力してください</p>";
         } else {
             for (let i = 1; i < 100; i++){
-                if (i % fizz === 0 && i % buzz === 0) {
-                    output_result += "<a>FizzBuzz " + i.toString() + "</a><br>";
-                } else if (i % fizz === 0) {
-                    output_result += "<a>Fizz " + i.toString() + "</a><br>";
-                } else if (i % buzz === 0 ){
-                    output_result += "<a>Buzz " + i.toString() + "</a><br>";
+                if (i % FIZZ === 0 && i % BUZZ === 0) {
+                    Output_Result += "FizzBuzz " + i.toString() + "<br>";
+                } else if (i % FIZZ === 0) {
+                    Output_Result += "Fizz " + i.toString() + "<br>";
+                } else if (i % BUZZ === 0 ){
+                    Output_Result += "Buzz " + i.toString() + "<br>";
                 }
             };
         };
-        document.getElementById("output_result").innerHTML = output_result;
+        document.getElementById("output_result").innerHTML = Output_Result;
     });
 }
