2c223ed31848406971a89adf9fc0b80c2a1f1477
raccoon <>
2019-10-28 11:54:47 +0900

    labelにタグ付け、変数名をキャメルケースに引数の名前を変更、バリデーション関数へと修正しました。

--- js/main.js
+++ js/main.js
@@ -1,35 +1,40 @@
 'use strict';
 
 {
-  const fizz_num = document.getElementById("fizz_num");
-  const buzz_num = document.getElementById("buzz_num");
+  const fizzNum = document.getElementById("fizz_num");
+  const buzzNum = document.getElementById("buzz_num");
   const btn = document.querySelector("button");
-  const result_area = document.getElementById("result_area");
+  const resultArea = document.getElementById("result_area");
 
-  function FizzBuzz (a, b) {
-    if (!(isNaN(a) || isNaN(b))) {
-      for (let i = 1; i <= 100; i++) {
-        const result = document.createElement('p');
-        if (i % a == 0 && i % b == 0) {
-          result.textContent = 'FizzBuzz ' + i;
-        } else if (i % a == 0) {
-          result.textContent = 'Fizz ' + i;
-        } else if (i % b == 0) {
-          result.textContent = 'Buzz ' + i;
-        } else {
-          result.textContent = '';
-        }
-        result_area.appendChild(result);
+  // 整数値バリデーション
+  function intValidate(target) {
+    return (!isNaN(Number(target)));
+  }
+
+  function fizzBuzz(fizz, buzz) {
+    for (let i = 1; i <= 100; i++) {
+      const result = document.createElement('p');
+      if (i % fizz == 0 && i % buzz == 0) {
+        result.textContent = 'FizzBuzz ' + i;
+      } else if (i % fizz == 0) {
+        result.textContent = 'Fizz ' + i;
+      } else if (i % buzz == 0) {
+        result.textContent = 'Buzz ' + i;
+      } else {
+        result.textContent = '';
       }
+      resultArea.appendChild(result);
+    }
+  }
+
+  btn.addEventListener('click', () => {
+    resultArea.innerHTML = '【出力】';
+    if (intValidate(fizzNum.value) && intValidate(buzzNum.value)) {
+      fizzBuzz(Number(fizzNum.value), Number(buzzNum.value));
     } else {
       const error = document.createElement('p');
       error.innerHTML = '整数値を入力してください';
-      result_area.appendChild(error);
+      resultArea.appendChild(error);
     }
-   }
-
-  btn.addEventListener('click', () => {
-    result_area.innerHTML = '【出力】';
-    FizzBuzz(Number(fizz_num.value), Number(buzz_num.value));
   });
 }
