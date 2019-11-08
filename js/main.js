276f92594e2eb80e2a5fe2f64638b82693c489f6
YuukiYamano <yuukiyamano1108@gmail.com>
2019-11-08 16:50:45 +0900

    fix

--- js/main.js
+++ js/main.js
@@ -1,42 +1,41 @@
 'usre strict'
 
-let num1 = document.getElementById('num1');
-let num2 = document.getElementById('num2');
-let action = document.getElementById('action');
-let div = document.getElementById('result');
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const ACTION = document.getElementById('action');
+const DIV = document.getElementById('result');
 
-let max_num = 100;
+let MAX_NUM = 100;
 
 function Remove(){
-  var result = document.getElementById('result');
+  const result = document.getElementById('result');
   if(result.hasChildNodes()){
-    for(var i=result.childNodes.length-1;i>=0;i++){
+    for(var i=result.childNodes.length - 1; i >= 0; i++){
       result.removeChild(result.childNodes[i]);
     }
   }
 }
 
-action.addEventListener('click',()=>{
+ACTION.addEventListener('click',()=>{
   let valuneText = parseFloat(num1.value) && parseFloat(num2.value);
 
-  // RemoveAll();
   
   if(Number.isInteger(valuneText)===true){
-    for (let i = 1; i<max_num;i++){
-      if(i%num1.value===0 & i%num2.value===0){
+    for (let i = 1; i < MAX_NUM; i++){
+      if(i % num1.value === 0 & i % num2.value === 0){
         const div = document.createElement('result');
         result.appendChild(div);
-        div.textContent = 'FizzBuzz\n'+i;
+        div.textContent = 'FizzBuzz' + i;
 
-      }else if(i%num1.value ===0){
+      }else if(i % num1.value === 0 ){
         const div = document.createElement('result');
         result.appendChild(div);
-        div.textContent = 'Fizz\n'+i;
+        div.textContent = 'Fizz\n' + i;
 
-      }else if(i&num2.value===0){
+      }else if(i & num2.value === 0 ){
         const div = document.createElement('result');
         result.appendChild(div);
-        div.textContent = 'Fizz\n'+i;
+        div.textContent = 'Fizz\n' + i;
       }
     }
 
